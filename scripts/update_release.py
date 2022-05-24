1e121d337f439d5e6045ebc35c58bd3026e601af
Dima Alexeenko <dilex@macpaw.com>
2022-05-24 11:46:35 +0300

    feat: test dat-778 release updater

--- scripts/update_release.py
+++ scripts/update_release.py
@@ -9,32 +9,27 @@ def changer(m):
     dat = m.group(0)
     return f'[{dat}](https://github.com/dilex42/rp-test/issues/{dat})'
     
-
-# repo = "dilex42/rp-test"
-repo = sys.argv[1]
-print(repo)
 GH_TOKEN = os.environ["GH_TOKEN"]
-
-url = f"https://api.github.com/repos/{repo}/releases/latest"
+REPO = sys.argv[1]
+print(f"Executing on repo {REPO}")
 headers = {
     "Accept":"application/vnd.github.v3+json",
     "Authorization": f"Bearer {GH_TOKEN}"
 }
 
-resp = requests.get(url,headers=headers)
-print(resp)
-print(resp.json())
-resp_json = resp.json()
-release_id = resp_json["id"]
-release_body = resp_json["body"]
-print(release_id)
-print(release_body)
+
+url = f"https://api.github.com/repos/{REPO}/releases/latest"
+response = requests.get(url,headers=headers)
+print(f"Fetching last release. Status {response.status_code}")
+response_json = response.json()
+release_id = response_json["id"]
+release_body = response_json["body"]
+print(f"Found release with id {release_id}")
 
 payload = {
     "body": re.sub("(?<!\[)\\b(?:(?:DAT)|(?:dat))-\d+\\b(?![\w\s]*[\])])",changer,release_body)
 }
 
-patch_url = f"https://api.github.com/repos/{repo}/releases/{release_id}"
-resp_p = requests.patch(patch_url,headers=headers,data=json.dumps(payload))
-print(resp_p)
-print(resp_p.text)
+patch_url = f"https://api.github.com/repos/{REPO}/releases/{release_id}"
+patch_response = requests.patch(patch_url,headers=headers,data=json.dumps(payload))
+print(f"Updating release. Status {patch_response.status_code}")
